09a592e42d471f56d0bb5dec8ee9487d928425cb
indiegamer3 <ftwergpust@gmail.com>
2026-01-21 17:20:49 +0900

    testing2

--- Assets/Scripts/Player_move.cs
+++ Assets/Scripts/Player_move.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
       bool canmove = true;
       bool ishitarea = false;
       float h;
-      bool github = false;
+      bool githubing;
       float v;
       // Start is called once before the first execution of Update after the MonoBehaviour is created
       void Start()
