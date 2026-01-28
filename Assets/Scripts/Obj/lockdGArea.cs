d5459052c645b49b9e3feb5f38394189c093ee63
indiegamer3 <ftwergpust@gmail.com>
2026-01-28 15:19:22 +0900

    Player Rigging

--- Assets/Scripts/Obj/lockdGArea.cs
+++ Assets/Scripts/Obj/lockdGArea.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class lockdGArea : MonoBehaviour
+public class lockdGArea : MonoBehaviour // 오브젝트 구분 역할
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
